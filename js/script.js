171aeb7daa3750fa41d83b52fc5f72a3844f3050
Dmitry Skok <dimas.skok95@gmail.com>
2021-12-17 18:15:06 +0100

    Add class active to the clicked link

--- js/script.js
+++ js/script.js
@@ -4,17 +4,21 @@
 //     console.log('links:', links);
 //   });
 
-const titleClickHandler = function(){
+const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-    /* remove class 'active' from all article links  */
+    /*[DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
         for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /*[IN PROGRESS] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    // console.log('clickedElement:', clickedElement);
+    // console.log('clickedElement (with plus): ' + clickedElement);
 
-    /* remove class 'active' from all articles */
+    /*[DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
         for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
