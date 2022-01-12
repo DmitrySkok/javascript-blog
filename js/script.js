da4315cc819e9fa91dfeca06cdb0b9be8a56b6d5
Dmitry Skok <dimas.skok95@gmail.com>
2022-01-12 18:46:11 +0100

    Done generate tags in the left side

--- js/script.js
+++ js/script.js
@@ -43,7 +43,8 @@ const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles',
   optArticleTagsSelector = '.post-tags .list',
-  optPostAuthorSelector = '.post-author';
+  optPostAuthorSelector = '.post-author',
+  optTagsListSelector = '.tags.list';
 
 function generateTitleLinks(customSelector = '') {
   /*[DONE] remove contents of titleList */
@@ -79,6 +80,8 @@ function generateTitleLinks(customSelector = '') {
 generateTitleLinks();
 
 function generateTags(){
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
   /*[DONE] find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
   /*[DONE] START LOOP: for every article: */
@@ -99,13 +102,22 @@ function generateTags(){
       const linkHTML = '<li><a href="#tag-' + tag + '">'+ tag +'</a></li>';
       /*[DONE] add generated code to html variable */
       html = html + linkHTML + ' ';
+      /* [NEW] check if this link is NOT already in allTags */
+      if(allTags.indexOf(linkHTML) == -1){
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+      }
     }
+
     /*[DONE] END LOOP: for each tag */
 
     /*[DONE] insert HTML of all the links into the tags wrapper */
     tagsWrapper.innerHTML = html;
-  }
-  /*[DONE] END LOOP: for every article: */
+  } /*[DONE] END LOOP: for every article: */
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector(optTagsListSelector);
+  /* [NEW] add html from allTags to tagList */
+  tagList.innerHTML = allTags.join(' ');
 }
 
 generateTags();
